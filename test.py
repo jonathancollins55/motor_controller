c5b8108ebeb76dc2142b7e1edbadcf296c4529f2
Jonathan Collins <jonathancollins55@gmail.com>
2023-02-11 10:49:49 -0500

    Arm function

--- test.py
+++ test.py
@@ -1,36 +1,66 @@
 import lgpio
+# import RPi.GPIO as gpio
 import time
 
 MOTOR = 12
 FREQ = 50
+MAX_SIGNAL = 10
+MIN_SIGNAL = 3.5
+STEP_SIZE = .5
 
 # gpio.setmode(gpio.BOARD)
 # gpio.setup(MOTOR, gpio.OUT)
 h = lgpio.gpiochip_open(0)
 
+def main():
+    arm()
 
-print("Frequency 1")
-lgpio.tx_pwm(h, MOTOR, FREQ, 10)
-time.sleep(2)
+    time.sleep(5)
 
-print("Frequency 2")
-lgpio.tx_pwm(h, MOTOR, FREQ, 10)
-time.sleep(2)
+    testMotorMovement()
 
-print("Frequency 3")
-lgpio.tx_pwm(h, MOTOR, FREQ, 5)
-time.sleep(2)
+    accelerate()
 
-lgpio.gpiochip_close(h)
+    lgpio.gpiochip_close(h)
 
-# try:
-#     while True:
-#         lgpio.tx_pwm(h, MOTOR, FREQ, 50)
-#         time.sleep(10)
+# def calibrate():
+    
+#     return
 
-#         # lgpio.tx_pwm(h, MOTOR, FREQ, 100)
-#         # time.sleep(10)
+def accelerate():
+    for i in range(MIN_SIGNAL,MAX_SIGNAL,STEP_SIZE):
+        lgpio.tx_pwm(h, MOTOR, FREQ, i)
+        time.sleep(1)
 
-# except KeyboardInterrupt:
-#     lgpio.tx_pwm(h, MOTOR, FREQ, 50)
-#     lgpio.gpiochip_close(h)
+def testMotorMovement():
+    print("Frequency 1")
+    lgpio.tx_pwm(h, MOTOR, FREQ, 10)
+    time.sleep(3)
+
+    print("Frequency 2")
+    lgpio.tx_pwm(h, MOTOR, FREQ, 10)
+    time.sleep(3)
+
+    print("Frequency 3")
+    lgpio.tx_pwm(h, MOTOR, FREQ, 5)
+    time.sleep(3)
+
+def arm():
+    turn_off_power = input("Disconnect Power then press any key...")
+
+    print("Now calibrating ESC")
+    print("Now writing maximum output...")
+
+    lgpio.tx_pwm(h, MOTOR, FREQ, MAX_SIGNAL)
+    turn_on_power = input("Turn on power source, then wait 2 seconds and press any key")
+
+
+    print("Sending minimum output")
+    lgpio.tx_pwm(h, MOTOR, FREQ, MIN_SIGNAL)
+
+    time.sleep(2)
+
+    print("Finished Arming")
+
+if __name__ == "__main__":
+    main()
