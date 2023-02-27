b685cda157221d6a773cf282eca4d21c7342ea61
Jonathan Collins <jonathan@onetouchbilling.com>
2023-02-26 19:49:48 -0500

    sensor output

--- sensor.py
+++ sensor.py
@@ -13,7 +13,8 @@ import Adafruit_GPIO.I2C as I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 while True:
-    print("Quaternion: {}".format(sensor.quaternion))
+    #print("Quaternion: {}".format(sensor.quaternion))
+    print("Euler: {}".format(sensor.gyro))
     time.sleep(1)
 
 # i2c = I2C
