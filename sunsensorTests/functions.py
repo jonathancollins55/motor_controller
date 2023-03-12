65c1186ed72f5c9a2743940f91a52ade105a273c
gkim65 <gracek1459@gmail.com>
2023-03-11 22:00:48 -0500

    small error

--- sunsensorTests/functions.py
+++ sunsensorTests/functions.py
@@ -133,10 +133,10 @@ def angleMeasure():
         offsets = [int(i) for i in offsets.split(",")]
         print(offsets)
         for mos1 in range(8):
-                values[0][mos1] = max-readMux_1(mos1)+offsets[mos1] #max[mos1]-readMux_1(mos1)
+                values[0][mos1] = max-readMux_1(mos1)-offsets[mos1] #max[mos1]-readMux_1(mos1)
                 values[1][mos1] = (8-mos1)*(-1)
         for mos2 in range(8):
-                values[0][mos2+8] = max - readMux_2(mos2)+offsets[mos2+8] # max[mos2+8] - readMux_2(mos2)
+                values[0][mos2+8] = max - readMux_2(mos2)-offsets[mos2+8] # max[mos2+8] - readMux_2(mos2)
                 values[1][mos2+8] = mos2+1
         total_current_distance = 0
         total_current = 0
