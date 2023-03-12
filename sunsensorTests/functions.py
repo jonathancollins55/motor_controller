fc57ec1a2f5c7a99d1d73351a9f065ceb384e76e
gkim65 <gracek1459@gmail.com>
2023-03-11 23:47:16 -0500

    small error

--- sunsensorTests/functions.py
+++ sunsensorTests/functions.py
@@ -176,7 +176,7 @@ def angleMeasure_regular():
         else:
                 normalized_current_distance = (total_current_distance/total_current)
                 angle = atan(normalized_current_distance/(height-3.2))*4068/(71*-1)
-                return angle + 18
+                return angle
 
 # Honestly, don't think we need this; from trial and error I got 65472 for max values for most
 def initialize_whenDark():
